a9f94e1352b716fdc944329ee8a4954f6a6e73fc
Claudiu <ionel.claudiu394@gmail.com>
2021-07-27 17:31:13 +0200

    registration with hashed password

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 const express = require('express');
 const cors = require('cors');
-
-var app = express();
+const app = express();
+const bcrypt = require('bcrypt')
 app.use(cors());
 app.use(express.json());
 
-var db = mysql.createConnection({
+const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'codeUntilTheEnd94!',
@@ -23,23 +23,21 @@ db.connect((err) => {
 })
 
 app.post('/register', (req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-
-  db.query('INSERT INTO users (username, email, password) VALUES(?, ?, ?);', [username, email, password],
-    (err, result) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send('User registered');
-      }
-    })
+  const { username, email, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    db.query('INSERT INTO users (username, email, password) VALUES(?, ?, ?);', [username, email, hash],
+      (err, result) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send('User registered');
+        }
+      })
+  })
 })
 
-app.get('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
   db.query('SELECT id, username from users u WHERE u.username = ? AND u.password = ?', [username, password], (err, result) => {
     if (err) {
